1c55117b64b3572a829bc4ae04882806110c1972
rvba <mil01@free.fr>
2017-04-08 20:58:02 +0200

    ether: add ether

--- lustre_lua.c
+++ lustre_lua.c
@@ -34,6 +34,10 @@
 #include "stone_spline_lua.h"
 #endif
 
+#ifdef HAVE_ETHERDREAM
+#include "etherdream_lua.h"
+#endif
+
 lua_State *LU_LUA_STATE = NULL;
 char LU_FILE_PATH[4096];
 int lua_file_open = 0;
@@ -288,6 +292,10 @@ int lustre_init( void)
 
 	#ifdef HAVE_TINYSPLINE
 	lua_stone_spline_register( LU_LUA_STATE);
+	#endif
+
+	#ifdef HAVE_ETHERDREAM
+	lua_etherdream_register( LU_LUA_STATE);
 	#endif
 
 	STONE_BUILD_FUNCTION = lu_lib_object_build;
